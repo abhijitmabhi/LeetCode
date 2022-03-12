56d54e1c693aa4fe9df3bff7db00a429f06582ff
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2022-03-12 01:38:55 +0100

    Merged

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,25 +12,9 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ContainsDuplicate(new int[] { 1, 2, 3, 5, 5 }));
-            Console.ReadKey();
+            
         }
 
-        public static bool ContainsDuplicate(int[] nums)
-        {
-            HashSet<int> myHash = new HashSet<int>();
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (!myHash.Add(nums[i]))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-
-        }
 
     }
 }
