ec85494bf5f0f163ff73de54f4915a7f2656473c
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2022-03-12 01:37:53 +0100

    Problem Solved 217

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -12,72 +12,25 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine(IsPalindrome(121));
+            Console.WriteLine(ContainsDuplicate(new int[] { 1, 2, 3, 5, 5 }));
             Console.ReadKey();
         }
 
-        public static bool IsPalindrome(int x)
+        public static bool ContainsDuplicate(int[] nums)
         {
-            if (x < 0)
+            HashSet<int> myHash = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                return false;
-            }
-
-            int number = x;
-            int reverse = 0;
-
-            while (x != 0)
-            {
-                int reminder = x % 10;
-                reverse = reverse * 10 + reminder;
-                x /= 10;
-
-            }
-            return reverse == number ? true : false;
-            Console.WriteLine("{0}", IsValid("(({}[({})]))"));
-=======
-            Console.WriteLine("{0}", RemoveDuplicates(new int[] {1,1,2,3,5,5}));
->>>>>>> Problem26_RemoveDuplicateFromSortedArray
-            Console.ReadKey();
-        }
-
-
-        public static int RemoveDuplicates(int[] nums)
-        {
-            int k = 0;
-
-            for(int i = 1; i < nums.Length; i++)
-            {
-                if(nums[k] != nums[i])
+                if (!myHash.Add(nums[i]))
                 {
-                    k++;
-                    nums[k] = nums[i];
+                    return true;
                 }
             }
 
-<<<<<<< HEAD
-            return endings.Count == 0;
+            return false;
 
-            Console.Write("[{0}]", string.Join(", ", PlusOne(new int[] { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6 })));
-            Console.ReadKey();
         }
 
-        public static int[] PlusOne(int[] digits)
-        {
-            string joined = String.Empty;
-            foreach(int digit in digits)
-            {
-                joined += digit.ToString();
-            }
-            BigInteger number = BigInteger.Parse(joined);
-            number++;
-            var arr = number.ToString().Select(x =>int.Parse(x.ToString())).ToArray();
-            return arr;
-
-=======
-            return k+1;
->>>>>>> Problem26_RemoveDuplicateFromSortedArray
-        }
     }
 }
