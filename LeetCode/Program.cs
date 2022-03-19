89b500f14e0c7bcb14a35bcab7457498a5e32f7d
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2022-03-19 23:44:49 +0100

    Merged

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,28 +6,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RemoveElement(new int[] {3,2,2,3}, 3));
-            Console.ReadKey();
-        }
-
-        public static int RemoveElement(int[] nums, int val)
-        {
-            int k = 0;
-
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (nums[i] != val)
-                {
-                    nums[k] = nums[i];
-                    k++;
-                }
-            }
-
-            return k;
 
         }
-
-
 
 
     }
