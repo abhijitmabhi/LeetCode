29ed81c4cb87534a9897f8af6046564742cbc109
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2022-04-15 11:32:40 +0200

    Problem Solved

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,7 +7,21 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine(SearchInsert(new int[] { 1, 3, 5 ,6}, 4));
+            Console.ReadKey();
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == target || nums[i] > target)
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length;
         }
 
 
