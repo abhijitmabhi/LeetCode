2457fb5e65ed19b83b2c78af87dbb40cfd140e4b
Abhi, Abhijit M <abhijit.abhi@cgi.com>
2023-12-28 10:45:42 +0100

    program solved

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,7 +7,36 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-         
+            DeleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3))))));
+        }
+
+        private static ListNode DeleteDuplicates(ListNode head)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            if(head.next == null)
+            {
+                return head;
+            }
+
+            ListNode current = head;
+
+            while (current.next != null)
+            {
+                if (current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
         }
     }
 }
